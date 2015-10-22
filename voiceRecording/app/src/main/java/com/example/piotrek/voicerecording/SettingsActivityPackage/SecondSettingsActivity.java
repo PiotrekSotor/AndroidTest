2a2298773608907ef40737fd6f636df47999a405
PiotrekSotor <piotrsotor@gmail.com>
2015-10-22 15:08:39 +0200

    [voicerecorder] kombinowanie z menu opcji

--- voiceRecording/app/src/main/java/com/example/piotrek/voicerecording/SettingsActivityPackage/SecondSettingsActivity.java
+++ voiceRecording/app/src/main/java/com/example/piotrek/voicerecording/SettingsActivityPackage/SecondSettingsActivity.java
@@ -2,8 +2,10 @@ package com.example.piotrek.voicerecording.SettingsActivityPackage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.piotrek.voicerecording.R;
 
@@ -12,13 +14,23 @@ import java.lang.reflect.AccessibleObject;
 /**
  * Created by Piotrek on 2015-10-21.
  */
-public class SecondSettingsActivity extends Activity {
+public class SecondSettingsActivity extends Activity implements Preference.OnPreferenceClickListener{
 
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         Log.e(this.getClass().getName(), "On Create");
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+
+        if (getResources().getString(R.string.list_pref_filter_unify_mode_key).equals(preference.getKey()))
+        {
+            Toast.makeText(this,"kliknięto na load profile :D",Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     public static class PrefsFragment extends PreferenceFragment {
